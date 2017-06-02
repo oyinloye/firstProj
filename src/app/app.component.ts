a885c076632f9684584092a1a2653b1133888869
Dapo <oyinloyedapo+GitHub@gmail.com>
2017-06-02 16:36:47 -0500

    1st push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+      { id: 11, name: 'David'},
+      { id: 12, name: 'Eric'},
+      { id: 13, name: 'Chris'},
+      { id: 14, name: 'Daley'},
+      { id: 15, name: 'Tony'},
+      { id: 16, name: 'Paul'},
+      { id: 17, name: 'Ander'},
+      { id: 18, name: 'Marcus'},
+      { id: 19, name: 'Antoine'},
+      { id: 20, name: 'Zlatan'}
+
+    ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onselect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  };
+export class Hero {
+  id: number;
+  name: string;
 }
